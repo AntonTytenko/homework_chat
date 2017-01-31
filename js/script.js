38b5371198cb4b4cc9a5c5cd48817f85c1be28a6
Anton Tytenko <potiomkin.anton@yandex.ua>
2017-02-01 00:35:22 +0200

    update_2

--- js/script.js
+++ js/script.js
@@ -3,34 +3,27 @@ var message = document.getElementById('textMessage');
 	message.onkeypress = function(el) {
 		//Количество набраных символов
 		document.getElementById('characters').innerHTML = message.value.length;
-		document.getElementById("counter").value = el.value.split(" ").length;}
+		
+		document.getElementById("letters").innerHTML = message.match(/\w/);
+		}
 	
 	//При нажатии на кнопку отправляеться сообщение
 	var subm = document.getElementById('submit');
-	subm.onclick = function() {
+	subm.onclick = addMessage;
+	function addMessage() {
 		var message = document.getElementById('textMessage');
 		if (message.value != '') {
 			var chat = document.getElementById('textChat');
 			var nel=document.createElement('p');
-			nel.innerHTML = "<span class='userName'>You</span>: "+ message.value + "<spsn class='time' id='time'></span>";
+			nel.innerHTML = "<span class='userName'>You</span>: "+ message.value + "<spsn class='time'>"+time()+"</span>";
 			chat.appendChild(nel);
 			message.value = '';
 		}
 	}
 	
 	//При нажатии на клавишу Enter отпралвяеться сообщение
-	message.onkeyup = function (e) {
-		if (message.value != '') {
-		if (e.keyCode === 13) {
-			
-			var chat = document.getElementById('textChat');
-			var nel=document.createElement('p');
-			nel.innerHTML = "<span class='userName'>You</span>: "+ message.value + "<spsn class='time' id='time'></span>";
-			chat.appendChild(nel);
-			message.value = '';
-		}
-		}
-	}
+	message.onkeyup = function (e) {if (e.keyCode === 13) {addMessage();}}
+
 	//Функция для добавление своего имени в список онлайн
 	var name = document.getElementById('name');
 	name.onkeyup = function (event) {
@@ -48,13 +41,20 @@ var message = document.getElementById('textMessage');
 	var ul = document.getElementById('peoply');
 	var li = ul.getElementsByTagName('li');
 	countPeoply.textContent = li.length;
-	
+
+//Функция времени (добавления времени в сообщения).	
 function time() {	
 	var date = new Date();
 	var m = date.getMinutes();
 	var h = date.getHours();
 	var s = date.getSeconds();
-	var time = document.getElementsByClassName('time');
-	time.innerHTML  = h +":"+ m;
+	if (m < 10 ) m="0"+m;
+	if (h < 10 ) h="0"+h;
+	if (s < 10 ) s="0"+s;
+	return h +":"+ m;
 }
-time();
+
+setInterval(function (){document.getElementById('time').innerHTML = time()}, 1000);
+
+
+
